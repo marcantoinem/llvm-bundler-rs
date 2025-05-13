edd53205e489f9cbed79bd1abc8d958008cb8be7
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-12 21:41:30 -0400

    feat: add blocking file read to avoid compiling while it's downloading

--- src/bundler.rs
+++ src/bundler.rs
@@ -1,7 +1,7 @@
 use std::{
     env::set_var,
     error::Error,
-    fs::{create_dir, exists},
+    fs::{self, create_dir, exists},
     path::PathBuf,
     time::Duration,
 };
@@ -14,19 +14,22 @@ use tar::Archive;
 const LINUX_ARTIFACT_URL: &str =
     "https://github.com/marcantoinem/llvm-bundler-rs/releases/download/v0.0.1/linux-x64.tar.xz";
 const LLVM_CACHE_PREFIX: &str = "llvm";
-
-fn decompress_tar_xz_stream(data: Bytes) -> Result<(), Box<dyn std::error::Error>> {
-    let cursor = std::io::Cursor::new(data);
-    let decoder = XzDecoder::new_parallel(cursor);
-    let mut archive = Archive::new(decoder);
-    archive.unpack(data_local_dir().ok_or("System not supported".to_string())?)?;
-    Ok(())
-}
+const FINISH_FILE_MUTEX: &str = "complete";
 
 pub fn llvm_path() -> Result<PathBuf, String> {
     data_local_dir()
         .map(|p| p.join(LLVM_CACHE_PREFIX))
         .ok_or("System not supported".to_string())
+}
+
+fn decompress_tar_xz_stream(data: Bytes) -> Result<(), Box<dyn std::error::Error>> {
+    let cursor = std::io::Cursor::new(data);
+    let decoder = XzDecoder::new_parallel(cursor);
+    let mut archive = Archive::new(decoder);
+    let local_dir = data_local_dir().ok_or("System not supported".to_string())?;
+    archive.unpack(&local_dir)?;
+    fs::write(llvm_path()?.join(FINISH_FILE_MUTEX), b"")?;
+    Ok(())
 }
 
 pub fn bundle_cache() -> Result<(), Box<dyn Error>> {
@@ -38,6 +41,11 @@ pub fn bundle_cache() -> Result<(), Box<dyn Error>> {
             .build()?;
         let response = client.get(LINUX_ARTIFACT_URL).send()?.bytes()?;
         decompress_tar_xz_stream(response)?;
+    } else if !exists(llvm_path.join(FINISH_FILE_MUTEX)).unwrap_or(false) {
+        // Is already downloading and extracting
+        while !exists(llvm_path.join(FINISH_FILE_MUTEX)).unwrap_or(false) {
+            std::thread::sleep(Duration::from_secs(1));
+        }
     }
     let libclang_path = llvm_path.join("lib");
     unsafe {
