f8641a5e697464282a9ede158afb5ab78302ce81
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-07-24 12:01:21 -0400

    chore: update build llvm to support more of clang

--- build_llvm.sh
+++ build_llvm.sh
@@ -17,7 +17,7 @@ mkdir -p build
 cmake -S llvm -B build -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=OFF \
-  -DLLVM_ENABLE_PROJECTS="clang;mlir;compiler-rt" \
+  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;mlir;compiler-rt" \
   -DLLVM_TARGETS_TO_BUILD="X86" \
   -DLLVM_BUILD_TESTS=OFF \
   -DLLVM_INCLUDE_TESTS=OFF \
